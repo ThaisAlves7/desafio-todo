55f6a0ecce4724440aa82ec4a38a0a0aa2ddc49e
ThaisAlves7 <75734682+ThaisAlves7@users.noreply.github.com>
2023-12-24 01:30:56 -0300

    Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import "./global.css";
+import styles from "./App.module.css";
 
 import { Header } from "./Components/Header";
+import { Input } from "./Components/Input";
 
 export function App() {
   return (
     <main>
       <Header />
+
+      <section className={styles.content}>
+        <div className={styles.taskInfoContainer}>
+          <Input />
+
+          
+        </div>
+      </section>
     </main>
   );
 }
